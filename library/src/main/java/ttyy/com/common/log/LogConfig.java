c59b318a9a5463d710db4c8107d2d7cf0ec35929
hujinqi <hujinqi@huaiye.com>
2017-03-28 21:12:39 +0800

    add logging module

---
+++ library/src/main/java/ttyy/com/common/log/LogConfig.java
@@ -0,0 +1,79 @@
+package ttyy.com.common.log;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * author: admin
+ * date: 2017/03/28
+ * version: 0
+ * mail: secret
+ * desc: LogConfig
+ */
+
+public class LogConfig {
+
+    private LogConfig(){
+
+    }
+
+    boolean isDebug = true;
+
+    Set<__$logging> loggings = new HashSet<__$logging>() {
+        {
+            add(__RuntimeLog.INSTANCE);
+            add(__ExternalLog.INSTANCE);
+        }
+    };
+
+    private static class Holder{
+        static LogConfig INSTANCE = new LogConfig();
+    }
+
+    public static LogConfig getInstance(){
+        return Holder.INSTANCE;
+    }
+
+    public LogConfig setIsDebug(boolean value) {
+        this.isDebug = value;
+        return this;
+    }
+
+    public LogConfig setEnableExternalLog(boolean value) {
+        if (value) {
+            if(!loggings.contains(__ExternalLog.INSTANCE)){
+                loggings.add(__ExternalLog.INSTANCE);
+            }
+        } else {
+            if(loggings.contains(__ExternalLog.INSTANCE)){
+                loggings.remove(__ExternalLog.INSTANCE);
+            }
+        }
+        return this;
+    }
+
+    public LogConfig setExternalLogDir(File mFile) {
+        setEnableExternalLog(mFile != null);
+        __ExternalLog.INSTANCE.setExternalLogDir(mFile);
+        return this;
+    }
+
+    public LogConfig setEnableRuntimeLog(boolean value) {
+        if (value) {
+            if(!loggings.contains(__RuntimeLog.INSTANCE)){
+                loggings.add(__RuntimeLog.INSTANCE);
+            }
+        } else {
+            if(loggings.contains(__RuntimeLog.INSTANCE)){
+                loggings.remove(__RuntimeLog.INSTANCE);
+            }
+        }
+        return this;
+    }
+
+    public boolean isDebug(){
+        return this.isDebug;
+    }
+
+}
--- library/src/main/java/ttyy/com/common/log/log.java
+++ library/src/main/java/ttyy/com/common/log/log.java
@@ -13,14 +13,14 @@ import android.util.Log;
 public class log {
 
     public static void d(String tag, String msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         print(Log.DEBUG, tag, msg);
     }
 
     public static void $d(String tag, Throwable msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         String msgStr = Log.getStackTraceString(msg);
@@ -28,14 +28,14 @@ public class log {
     }
 
     public static void i(String tag, String msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         print(Log.INFO, tag, msg);
     }
 
     public static void $i(String tag, Throwable msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         String msgStr = Log.getStackTraceString(msg);
@@ -43,14 +43,14 @@ public class log {
     }
 
     public static void e(String tag, String msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         print(Log.ERROR, tag, msg);
     }
 
     public static void $e(String tag, Throwable msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         String msgStr = Log.getStackTraceString(msg);
@@ -58,14 +58,14 @@ public class log {
     }
 
     public static void w(String tag, String msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         print(Log.WARN, tag, msg);
     }
 
     public static void $w(String tag, Throwable msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
         String msgStr = Log.getStackTraceString(msg);
@@ -73,10 +73,10 @@ public class log {
     }
 
     public static void print(int l, String tag, String msg){
-        if(!logConfig.isDebug){
+        if(!LogConfig.getInstance().isDebug()){
             return;
         }
-        for(__$logging tmp : logConfig.loggings){
+        for(__$logging tmp : LogConfig.getInstance().loggings){
             tmp.printLog(l, tag, msg);
         }
     }
