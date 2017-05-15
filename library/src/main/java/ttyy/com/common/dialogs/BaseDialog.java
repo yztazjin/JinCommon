e84b20d2429f5fb9d2999cf524fff0831db71f97
yztazjin <hudadada0305@gmail.com>
2017-05-15 16:40:19 +0800

    Update BaseDialog.java

--- library/src/main/java/ttyy/com/common/dialogs/BaseDialog.java
+++ library/src/main/java/ttyy/com/common/dialogs/BaseDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 /**
@@ -17,9 +18,11 @@ import android.widget.LinearLayout;
 public abstract class BaseDialog extends Dialog{
 
     protected LinearLayout mRootView;
+    protected BaseDialogBuilder mBuilder;
 
     public BaseDialog(BaseDialogBuilder builder) {
         super(builder.getContext());
+        mBuilder = builder;
 
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -27,13 +30,26 @@ public abstract class BaseDialog extends Dialog{
         mRootView = new LinearLayout(builder.getContext());
         mRootView.setOrientation(LinearLayout.VERTICAL);
         mRootView.setBackground(builder.getWindowBackgroundColorDrawable());
+
+        onRootViewInitialized(mRootView);
+
         setContentView(mRootView);
+
+        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.width = builder.getWindowWidth();
+        attrs.height = builder.getWindowHeight();
+        attrs.dimAmount = builder.getDim();
+        getWindow().setAttributes(attrs);
     }
 
     protected abstract void onRootViewInitialized(LinearLayout mRootView);
 
     public LinearLayout getRootView(){
         return mRootView;
+    }
+
+    public <T extends BaseDialogBuilder> T getBuilder(){
+        return (T)mBuilder;
     }
 
 }
