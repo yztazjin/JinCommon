0d583ae1a92f859d686e3c2e252609d7c91cf72f
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:57:13 +0800

    Create MsgDialogBuilder.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/msg/MsgDialogBuilder.java
@@ -0,0 +1,31 @@
+package ttyy.com.common.dialogs.msg;
+
+import android.content.Context;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: MsgDialogBuilder
+ */
+
+public class MsgDialogBuilder extends BaseDialogBuilder {
+
+    public MsgDialogBuilder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public <T extends BaseDialog> T build() {
+        return null;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
