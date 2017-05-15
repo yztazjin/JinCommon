febbefc499411984ad99396067ccc56826c75810
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:55:32 +0800

    Create AskDialog.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/ask/AskDialog.java
@@ -0,0 +1,26 @@
+package ttyy.com.common.dialogs.ask;
+
+import android.widget.LinearLayout;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: AskDialog
+ */
+
+public class AskDialog extends BaseDialog {
+
+    public AskDialog(BaseDialogBuilder builder) {
+        super(builder);
+    }
+
+    @Override
+    protected void onRootViewInitialized(LinearLayout mRootView) {
+
+    }
+}
