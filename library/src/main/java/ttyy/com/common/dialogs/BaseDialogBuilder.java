abb45751e449d825b78943fffc9b341c0d035a31
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:57:56 +0800

    Create BaseDialogBuilder.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/BaseDialogBuilder.java
@@ -0,0 +1,115 @@
+package ttyy.com.common.dialogs;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.view.WindowManager;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: BaseDialogBuilder
+ */
+
+public abstract class BaseDialogBuilder {
+
+    private int windowWidth;
+    private int windowHeight;
+    private float dim;
+
+    private int[] radius;// tl tr br bl
+
+    private int windowBackgroundColor;
+
+    private Context context;
+
+    public BaseDialogBuilder(Context context){
+        this.context = context;
+        windowWidth = (int) (context.getResources().getDisplayMetrics().widthPixels * 0.812f);
+        windowHeight = WindowManager.LayoutParams.WRAP_CONTENT;
+        windowBackgroundColor = Color.WHITE;
+
+        radius = new int[]{5,5,5,5};
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    public int getWindowWidth(){
+        return windowWidth;
+    }
+
+    public BaseDialogBuilder setWindowWidth(int value){
+        windowWidth = value;
+        return this;
+    }
+
+    public int getWindowHeight() {
+        return windowHeight;
+    }
+
+    public BaseDialogBuilder setWindowHeight(int value){
+        windowHeight = value;
+        return this;
+    }
+
+    public float getDim(){
+        return dim;
+    }
+
+    public BaseDialogBuilder setDim(float value){
+        dim = value;
+        return this;
+    }
+
+    public int getWindowBackgroundColor(){
+        return windowBackgroundColor;
+    }
+
+    public BaseDialogBuilder setWindowBackgroundColor(int color){
+        windowBackgroundColor = color;
+        return this;
+    }
+
+    protected Drawable getWindowBackgroundColorDrawable(){
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setColor(windowBackgroundColor);
+
+        drawable.setCornerRadii(new float[]{
+                radius[0],radius[0],
+                radius[1],radius[1],
+                radius[2],radius[2],
+                radius[3],radius[3],
+                radius[4],radius[4]});
+
+        return drawable;
+    }
+
+    public int[] getRadius(){
+        return radius;
+    }
+
+    public BaseDialogBuilder setRadius(int r){
+        radius[0] = r;
+        radius[1] = r;
+        radius[2] = r;
+        radius[3] = r;
+        return this;
+    }
+
+    public BaseDialogBuilder setRadius(int tl, int tr, int br, int bl){
+        radius[0] = tl;
+        radius[1] = tr;
+        radius[2] = br;
+        radius[3] = bl;
+        return this;
+    }
+
+    public abstract <T extends BaseDialog> T build();
+
+    public abstract void show();
+}
