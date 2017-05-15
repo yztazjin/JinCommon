581547558782271c3cf1be32ed746b288f6e748f
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:56:30 +0800

    Create ChoseDialogBuilder.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/chose/ChoseDialogBuilder.java
@@ -0,0 +1,31 @@
+package ttyy.com.common.dialogs.chose;
+
+import android.content.Context;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: ChoseDialogBuilder
+ */
+
+public class ChoseDialogBuilder extends BaseDialogBuilder {
+
+    public ChoseDialogBuilder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public <T extends BaseDialog> T build() {
+        return null;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
