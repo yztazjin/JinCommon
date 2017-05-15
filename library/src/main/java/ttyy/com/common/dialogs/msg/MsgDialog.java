a5eb4b39fe0854edec2549f794129269f52e3186
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:56:52 +0800

    Create MsgDialog.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/msg/MsgDialog.java
@@ -0,0 +1,26 @@
+package ttyy.com.common.dialogs.msg;
+
+import android.widget.LinearLayout;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: MsgDialog
+ */
+
+public class MsgDialog extends BaseDialog {
+
+    public MsgDialog(BaseDialogBuilder builder) {
+        super(builder);
+    }
+
+    @Override
+    protected void onRootViewInitialized(LinearLayout mRootView) {
+
+    }
+}
