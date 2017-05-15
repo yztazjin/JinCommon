d69897fec711eacc79f30d635f4e64de91e0cbc1
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:55:50 +0800

    Create AskDialogBuilder.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/ask/AskDialogBuilder.java
@@ -0,0 +1,31 @@
+package ttyy.com.common.dialogs.ask;
+
+import android.content.Context;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: AskDialogBuilder
+ */
+
+public class AskDialogBuilder extends BaseDialogBuilder {
+
+    public AskDialogBuilder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public <T extends BaseDialog> T build() {
+        return null;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
