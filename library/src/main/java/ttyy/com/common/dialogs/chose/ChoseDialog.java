4326707bc527c18fbff6cac58fa05d729c7b9b99
yztazjin <hudadada0305@gmail.com>
2017-05-15 14:56:13 +0800

    Create ChoseDialog.java

---
+++ library/src/main/java/ttyy/com/common/dialogs/chose/ChoseDialog.java
@@ -0,0 +1,26 @@
+package ttyy.com.common.dialogs.chose;
+
+import android.widget.LinearLayout;
+
+import ttyy.com.common.dialogs.BaseDialog;
+import ttyy.com.common.dialogs.BaseDialogBuilder;
+
+/**
+ * author: admin
+ * date: 2017/05/15
+ * version: 0
+ * mail: secret
+ * desc: ChoseDialog
+ */
+
+public class ChoseDialog extends BaseDialog {
+
+    public ChoseDialog(BaseDialogBuilder builder) {
+        super(builder);
+    }
+
+    @Override
+    protected void onRootViewInitialized(LinearLayout mRootView) {
+
+    }
+}
