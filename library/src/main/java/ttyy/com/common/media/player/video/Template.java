27413e5c230572406aa5ea6b027968f2da40dec4
yztazjin <hudadada0305@gmail.com>
2017-06-09 17:32:08 +0800

    Create Template.java

---
+++ library/src/main/java/ttyy/com/common/media/player/video/Template.java
@@ -0,0 +1,14 @@
+package ttyy.com.common.media.player.video;
+
+/**
+ * author: admin
+ * date: 2017/06/09
+ * version: 0
+ * mail: secret
+ * desc: Template
+ */
+
+public class Template {
+    // TO-DO
+    // Not Impl
+}
